b7e494b2c6afb09e7e0f4d86aee07e8bab701aa1
Ivaylo Moraliev <ivaylo.moraliev@gmail.com>
2022-08-23 22:48:04 +0300

    Add project files.

---
+++ 02.OddOccurences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToLower().Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!counts.ContainsKey(word))
+                {
+                    counts.Add(word, 0);
+                }
+
+                counts[word]++;
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+
+                }
+            }
+        }
+    }
+}
---
+++ 04.Largest3Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sorted = numbers.OrderByDescending(n => n).ToArray();
+
+            if (numbers.Length < 3)
+            {
+                Console.WriteLine(String.Join(" ", sorted));
+            }
+
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            }
+        }
+    }
+}
